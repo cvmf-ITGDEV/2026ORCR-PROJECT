c3c7945855ef736d357f9e6a42a262dc1bd7fa31
ITG DEV <glennbancolita@cvmfinance.com>
2026-01-06 13:47:59 +0800

    Updated server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -15,6 +15,8 @@ export function createClient() {
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
+
+    console.log('res:', res)
     return res;
   }
   
