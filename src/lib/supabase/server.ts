e4e6f3a91aabae92ff8d681fe14803d348cef4a8
ITG DEV <glennbancolita@cvmfinance.com>
2026-01-06 13:37:46 +0800

    Updated server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -10,7 +10,7 @@ type CookieOptions = {
 export function createClient() {
   console.log('Hakdog')
   const cookieStore = cookies();
-console.log('Cookie:', cookieStore)
+  console.log('Cookie:', cookieStore)
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
